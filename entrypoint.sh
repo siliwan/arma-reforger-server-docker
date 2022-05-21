047922415251c3dfd453178992b305837d79ce63
Lukas Huber <lukas.lh@gmx.ch>
2022-05-21 10:09:30 +0200

    Added ip to entrypoint for better debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export CONTAINER_IP=$(hostname -I | awk '{print $1}')
+echo "[System] IP of container: $CONTAINER_IP"
 echo "[System] Installing/Updating"
 steamcmd +force_install_dir /home/container +login anonymous +app_update 1874900 +quit
 echo "[System] Installation/Update finished"
